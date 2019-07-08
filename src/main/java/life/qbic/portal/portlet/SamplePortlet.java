ad0ebf01dfd278a1d4aadbdd43c11a3519b7a899
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-07-08 14:19:12 +0200

    Displays spaces in a textfield

--- src/main/java/life/qbic/portal/portlet/SamplePortlet.java
+++ src/main/java/life/qbic/portal/portlet/SamplePortlet.java
@@ -1,16 +1,20 @@
 package life.qbic.portal.portlet;
 
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.Space;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.HorizontalLayout;
 
+import com.vaadin.ui.TextField;
 import life.qbic.openbis.openbisclient.OpenBisClient;
 import life.qbic.portal.utils.ConfigurationManager;
 import life.qbic.portal.utils.ConfigurationManagerFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 /**
  * Entry point for portlet connection-tester. This class derives from {@link QBiCPortletUI}, which is found in the {@code portal-utils-lib} library.
@@ -30,13 +34,23 @@ public class SamplePortlet extends QBiCPortletUI {
 
 
         ConfigurationManager config = ConfigurationManagerFactory.getInstance();
+        StringBuilder stringBuilder = new StringBuilder()
         OpenBisClient obis = new OpenBisClient(config.getDataSourceUser(),
                 config.getDataSourcePassword(), config.getDataSourceUrl());
         obis.login();
-        obis.listSpaces();
-        
+        final TextField field = new TextField();
+        List<String> spaces = obis.listSpaces();
+        for (String space : spaces) {
+            stringBuilder.append(space);
+            stringBuilder.append("\n");
+        }
+
+        field.setValue(stringBuilder.toString());
         // TODO: generate content for your portlet
         //       this method returns any non-null layout to avoid a NullPointerException later on
-        return new HorizontalLayout();
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.addComponent(field);
+
+        return layout;
     }    
 }
